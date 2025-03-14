a57e096b029247c20989580de6e01651e580d534
Attila Horvath <sweeptic@gmail.com>
2025-03-14 15:10:12 +0100

    006 Understanding Requests

--- app.js
+++ app.js
@@ -1,20 +1,9 @@
-// core NodeJS modules;
-// http;
-// https;
-// fs;
-// path;
-// os
-
-// function createServer<typeof http.IncomingMessage, typeof http.ServerResponse>
-// (requestListener?: http.RequestListener<typeof http.IncomingMessage, typeof http.ServerResponse> | undefined): http.Server<...> (+1 overload)
-// Returns a new instance of Server.
-
 const http = require('http');
 
-// function requestListener(req, res) {}
-
 const server = http.createServer((req, res) => {
-  console.log('req', req);
+  console.log('req.url', req.url);
+  console.log('req.method', req.method);
+  console.log('req.headers', req.headers);
 
   res.writeHead(200, { 'Content-Type': 'application/json' });
   res.end(
