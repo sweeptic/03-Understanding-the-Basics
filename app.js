9e7a4c6030c453628e12ee031ce8b381785fc2dc
Attila Horvath <sweeptic@gmail.com>
2025-03-14 14:59:13 +0100

    __003 Creating a Node Server

--- app.js
+++ app.js
@@ -4,3 +4,24 @@
 // fs;
 // path;
 // os
+
+// function createServer<typeof http.IncomingMessage, typeof http.ServerResponse>
+// (requestListener?: http.RequestListener<typeof http.IncomingMessage, typeof http.ServerResponse> | undefined): http.Server<...> (+1 overload)
+// Returns a new instance of Server.
+
+const http = require('http');
+
+// function requestListener(req, res) {}
+
+const server = http.createServer((req, res) => {
+  console.log('req', req);
+
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.end(
+    JSON.stringify({
+      data: 'Hello World!',
+    })
+  );
+});
+
+server.listen(3000);
