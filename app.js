8eb45077766bbfa85789393a9972988f5c955da2
Attila Horvath <sweeptic@gmail.com>
2025-03-14 15:13:25 +0100

    007 Sending Responses

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  console.log('req.url', req.url);
-  console.log('req.method', req.method);
-  console.log('req.headers', req.headers);
-
   res.writeHead(200, { 'Content-Type': 'application/json' });
   res.end(
     JSON.stringify({
