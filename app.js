9574a01315d36741537181ed1ae7ef39bbf6fbb2
Attila Horvath <sweeptic@gmail.com>
2025-03-17 18:41:54 +0100

    013 Blocking and Non-Blocking Code

--- app.js
+++ app.js
@@ -26,10 +26,16 @@ const server = http.createServer((req, res) => {
       const parsedBody = Buffer.concat(body).toString();
       console.log('parsedBody', parsedBody);
       const message = parsedBody.split('=')[1];
-      fs.writeFileSync('message.txt', message);
-      res.statusCode = 302;
-      res.setHeader('Location', '/');
-      return res.end();
+      fs.writeFile('message.txt', message, (error) => {
+        console.log('DONE');
+        console.log('ERROR', error);
+
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        console.log('SET LOCATION');
+
+        return res.end();
+      });
     });
   }
   res.setHeader('Content-Type', 'text/html');
